188bd021d54ddc036fc1e54736a2b806679c3105
Alex Schumann <78286646+CobblerOfShoes@users.noreply.github.com>
2025-03-21 08:56:08 -0400

    Last few changes
    Added rate limiter and more comments

--- app.js
+++ app.js
@@ -5,14 +5,32 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
+// Hiding secrets!
 require('dotenv/config')
 
+// Rate limiter: taken from https://www.npmjs.com/package/express-rate-limit
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
+// Authentication -- registration and login
 const authRoute = require('./routes/auth')
 app.use('/api/auth', authRoute)
 
+// Listing users -- testing, should be removed in prod
 const usersRoute = require('./routes/users')
 app.use('/api/users', usersRoute)
 
+// Creating and viewing posts
 const postsRoute = require('./routes/posts')
 app.use('/api/posts', postsRoute)
 
